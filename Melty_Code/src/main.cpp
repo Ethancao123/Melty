daa9c738a551ab432e77367a837598fe5bd69159
Ethancao123 <46360828+Ethancao123@users.noreply.github.com>
2025-03-29 03:33:41 -0500

    ESC outline

--- Melty_Code/src/main.cpp
+++ Melty_Code/src/main.cpp
@@ -1,49 +1,66 @@
 #include <Arduino.h>
+#include <FastLED.h>
+
+#include "DShotESC.h"
+
 #include "elrs.hpp"
-#include "telem.hpp"
+#include "telemwifi.hpp"
 
 #define DEBUG
 
-bool failsafe = false;
+#define LED_PIN     D4
+#define CHIPSET     WS2812B
+#define NUM_LEDS    6
 
+CRGB leds[NUM_LEDS];
+
+#define ESC0_PIN      GPIO_NUM_5
+#define ESC1_PIN      GPIO_NUM_6
+DShotESC esc0;
+DShotESC esc1;
+
+bool failsafe = false;
 void setup() {
   #ifdef DEBUG
+  delay(1000);
   Serial.begin(115200);
   Serial.println("COM Serial initialized");
   #endif
   initELRS();
-  // initTelem();
+  initTelem();
+
+  FastLED.addLeds<WS2812B, LED_PIN>(leds, NUM_LEDS);
+
+  esc0.install(ESC0_PIN, RMT_CHANNEL_0);
+  esc1.install(ESC1_PIN, RMT_CHANNEL_1);
+  esc0.init();
+  esc1.init();
+  esc0.set3DMode(true);
+  esc1.set3DMode(true);
+  for (int i = 0; i < 5; i++)
+	{
+		esc0.beep(i);
+    esc1.beep(i);
+	}
 }
 
 void loop() {
   #ifdef DEBUG
-  printChannels();
-  if(failsafe)
-    Serial.println("FAILSAFE!");
+  // printChannels();
+  // if(failsafe)
+  //   Serial.println("FAILSAFE!");
   #endif
+  //stuff to loop even in failsafe
   failsafe = !crsf.isLinkUp();
+  FastLED.show();
   loopELRS();
-  if(failsafe)
+  if(failsafe) {
+    for(int i = 0; i < NUM_LEDS; i++)
+      leds[i] = CRGB::Red;
     return;
+  }
+  //stuff to not loop if in failsafe
+
+    
   
 }
-
-// #include "NimBLEDevice.h"
-
-// void setup() {
-//     NimBLEDevice::init("NimBLE");
-    
-//     NimBLEServer *pServer = NimBLEDevice::createServer();
-//     NimBLEService *pService = pServer->createService("ABCD");
-//     NimBLECharacteristic *pCharacteristic = pService->createCharacteristic("1234");
-    
-//     pService->start();
-//     pCharacteristic->setValue("Hello BLE");
-    
-//     NimBLEAdvertising *pAdvertising = NimBLEDevice::getAdvertising();
-//     pAdvertising->addServiceUUID("ABCD"); // advertise the UUID of our service
-//     pAdvertising->setName("NimBLE"); // advertise the device name
-//     pAdvertising->start(); 
-// }
-
-// void loop(){}
